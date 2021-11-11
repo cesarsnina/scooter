0cc04abcf5be1eefb7805f5cfcfe2c5329c295b6
cesarsnina <cesar.nina05@gmail.com>
2021-11-11 17:02:48 +0000

    added test for scooter

--- test/Scooter.test.js
+++ test/Scooter.test.js
@@ -1,8 +1,17 @@
-const Scooter = require("./../src/Scooter")
+const Scooter = require('./../src/Scooter');
 
-describe("Scooter class datatypes", () => {
-    test("Scooter has an ID", () => {
-        const scooter1 = new Scooter("117", "Queens", true, false);
-        expect(typeof scooter1.id).toBe("string")
-    }) 
-})
+describe('Scooter class data type', () => {
+    const scooter1 = new Scooter('117', 'Queens', true, false);
+    test('id should be a string', () => {
+        expect(typeof scooter1.id).toBe('string');
+    });
+    test('origin should be a string', () => {
+        expect(typeof scooter1.origin).toBe('string');
+    }); 
+    test('isCharged should be a boolean', () => {
+        expect(typeof scooter1.isCharged).toBe('boolean');
+    }); 
+    test('isDamaged should be a boolean', () => {
+        expect(typeof scooter1.isDamaged).toBe('boolean');
+    }); 
+});
