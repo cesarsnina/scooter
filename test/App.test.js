d7698cbdf67077e260cdf9ab871c2b48ac7f9c4c
CDenille <carringtondenille@gmail.com>
2021-11-11 22:07:52 +0000

    adds station test

--- test/App.test.js
+++ test/App.test.js
@@ -1,19 +1,19 @@
-const App = require("./../src/App")
-const User = require("./../src/User")
-const Scooter = require("./../src/Scooter")
-const Station = require("./../src/Station")
+// const App = require("./../src/App")
+// const User = require("./../src/User")
+// const Scooter = require("./../src/Scooter")
+// const Station = require("./../src/Station")
 
-describe("App class datatypes", () => {
-    const scooter1 = new Scooter("100", "The Bronx", true, false)
-    const scooter2 = new Scooter("101", "The Bronx", true, false)
-    const scooter3 = new Scooter("102", "The Bronx", true, false)
-    const bronxStation = new Station("The Bronx")
-    bronxStation.addScooter(scooter1)
-    bronxStation.addScooter(scooter2)
-    bronxStation.addScooter(scooter3)
+// describe("App class datatypes", () => {
+//     const scooter1 = new Scooter("100", "The Bronx", true, false)
+//     const scooter2 = new Scooter("101", "The Bronx", true, false)
+//     const scooter3 = new Scooter("102", "The Bronx", true, false)
+//     const bronxStation = new Station("The Bronx")
+//     bronxStation.addScooter(scooter1)
+//     bronxStation.addScooter(scooter2)
+//     bronxStation.addScooter(scooter3)
 
-    test("App returns scooter list at specified station", () => {
-        expect(App.scootersAtStation("The Bronx").length).toBe(3)
-    }) 
-    //add test for addUser without downloadApp
+    // test("App returns scooter list at specified station", () => {
+    //     expect(App.scootersAtStation("The Bronx").length).toBe(3)
+    // }) 
+    // add test for addUser without downloadApp
 })
