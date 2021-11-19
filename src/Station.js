42dfb7da9c1406502d7058e0aa4fb3150f19e5d1
Jonathan Ford <jwford@google.com>
2021-11-19 20:53:00 +0000

    scooters recharge at stations

--- src/Station.js
+++ src/Station.js
@@ -19,7 +19,21 @@ class Station {
             console.log(`scooter ${scooter.id} was added to ${this.cityName}`);
             this.scooters.push(scooter);
         }
-    };
-};
+    }
+
+    /**
+     * 
+     * @param {Scooter} scooter The scooter to be charged
+     */
+    async chargeScooter(scooter) {
+        if (scooter.isBroken) throw new Error('cannot charge broken scooter');
+
+        const amountToCharge = 100 - scooter.batteryLife;
+
+        await new Promise(resolve => setTimeout(resolve, amountToCharge * 1000));
+
+        console.log('scooter fully charged');
+    }
+}
 
 module.exports = Station;
--- src/practice2.js
+++ src/practice2.js
@@ -7,17 +7,22 @@ const App = require('./App')
 const admin = new App()
 const jorge = new User('Jorge', 'jorge@google.com', 28, 100)
 const westStation = new Station('DC')
+const eastStation = new Station('NYC');
 const scooter1 = new Scooter(1)
 
+async function main() {
 // Add scooter to station
 westStation.addScooter(scooter1)
-
 // User setup
 jorge.downloadApp()
 jorge.rentScooter(westStation)
 
 // passes in time used in minutes
-jorge.usesScooter(6000)
+await jorge.usesScooter(6000)
 
 // jorge.returns..
+jorge.returnScooter(eastStation);
+await eastStation.chargeScooter(scooter1);
+}
 
+main();
